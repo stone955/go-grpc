935ebc0c14c0618e6c2a8b00ba6f57a885c11296
stone <335612970@qq.com>
2020-06-18 14:32:29 +0800

    1.自签证书 2.CA证书

---
+++ client/crt_client.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc/credentials"
+	"io"
+	"log"
+	"sync"
+
+	"github/stone955/go-grpc/proto"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	cred, err := credentials.NewClientTLSFromFile("../crt/server.crt", "localhost")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 建立连接
+	conn, err := grpc.Dial("localhost:8001", grpc.WithTransportCredentials(cred))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		_ = conn.Close()
+	}()
+
+	// 获取客户端
+	client := proto.NewHelloServiceClient(conn)
+
+	// 单向流调用
+	request := &proto.HelloRequest{
+		Name: "sdy",
+	}
+
+	resp, err := client.Hello(context.TODO(), request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("response fullname: %v\n", resp.FullName)
+
+	// 双向流调用
+	// 获取流
+	stream, err := client.HelloStream(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 确保发送线程，接收线程都结束才退出
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	// 发送线程
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 10; i++ {
+			if err := stream.Send(&proto.HelloRequest{Name: "stone "}); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}()
+
+	// 接收线程
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 10; i++ {
+			resp, err := stream.Recv()
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				log.Fatal(err)
+			}
+			log.Printf("stream response fullname: %v\n", resp.FullName)
+		}
+	}()
+
+	wg.Wait()
+}
---
+++ server/crt_server.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"google.golang.org/grpc/credentials"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+
+	"github/stone955/go-grpc/internal/service"
+	"github/stone955/go-grpc/proto"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	cred, err := credentials.NewServerTLSFromFile("../crt/server.crt", "../crt/server.key")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 创建 grpc 服务
+	s := grpc.NewServer(grpc.Creds(cred))
+	// 注册服务
+	proto.RegisterHelloServiceServer(s, &service.HelloService{})
+
+	//  监听退出信号，优雅关闭
+	var wg sync.WaitGroup
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch)
+
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+		select {
+		case <-ch:
+			s.GracefulStop()
+		}
+	}()
+
+	// 监听端口
+	listen, err := net.Listen("tcp", ":8001")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 启动服务
+	if err := s.Serve(listen); err != nil {
+		log.Fatal(err)
+	}
+
+	wg.Wait()
+
+	log.Println("server graceful stopped")
+}
